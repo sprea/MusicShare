929eb09533cca810b47cecbf8cfe48f2c7c6b74a
Simone Spreafico <simo.sprea1907@gmail.com>
2020-06-01 13:13:35 +0200

    Aggiunta classe SeedData per aggiungere l'admin ed i ruoli

---
+++ Models/SeedData.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicShare.Models
+{
+    public static class SeedData
+    {
+        public static void Seed(UserManager<ApplicationUser> userManager, 
+            RoleManager<IdentityRole> roleManager)
+        {
+            SeedRoles(roleManager);
+            SeedUser(userManager);
+        }
+
+        public static void SeedUser(UserManager<ApplicationUser> userManager)
+        {
+            var AdminResult = userManager.FindByNameAsync("admin@musicshare.it").Result;
+
+            if(AdminResult == null)
+            {
+                ApplicationUser user = new ApplicationUser
+                {
+                    Email = "admin@musicshare.it",
+                    UserName = "admin@musicshare.it"
+                };
+
+                var result = userManager.CreateAsync(user, "Qwertyuiop123@").Result;
+
+                if (result.Succeeded)
+                    userManager.AddToRoleAsync(user, "Admin").Wait();
+            }
+        }
+
+        public static void SeedRoles(RoleManager<IdentityRole> roleManager)
+        {
+            if(!roleManager.RoleExistsAsync("Admin").Result)
+            {
+                roleManager.CreateAsync(new IdentityRole("Admin"));
+            }
+
+            if (!roleManager.RoleExistsAsync("User").Result)
+            {
+                roleManager.CreateAsync(new IdentityRole("User"));
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MusicShare.Models;
 
 namespace MusicShare
 {
@@ -13,7 +16,29 @@ namespace MusicShare
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+
+            var host = CreateHostBuilder(args).Build();
+
+            using(var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+
+                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+                    SeedData.Seed(userManager, roleManager);
+                }catch(Exception ex)
+                {
+                    ex.ToString();
+                }
+
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
