eee1ac10f47d60bc48d1808b6e3ca29236db0496
Simone Spreafico <simo.sprea1907@gmail.com>
2020-06-13 15:56:06 +0200

    Aggiunta sezione gestione utenti per amministratori

---
+++ Areas/Identity/Pages/Users/Delete.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MusicShare.Models;
+
+namespace MusicShare.Areas.Identity.Pages.Users
+{
+    [Authorize(Roles = "Admin")]
+    public class DeleteModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public DeleteModel(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public ApplicationUser Utente { get; set; }
+
+        public async Task<IActionResult> OnGet(string id)
+        {
+            if (id == null)
+                return NotFound();
+
+            Utente = _userManager.FindByIdAsync(id).Result;
+
+            if (Utente == null)
+                return NotFound();
+
+            var chech = await _userManager.DeleteAsync(Utente);
+
+            if (chech.Succeeded)
+                return RedirectToPage("./Index");
+
+            return Page();
+        }
+    }
+}
--- Areas/Identity/Pages/Users/Index.cshtml.cs
+++ Areas/Identity/Pages/Users/Index.cshtml.cs
@@ -1,17 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MusicShare.Models;
 
 namespace MusicShare.Areas.Identity.Pages.Users
 {
+    [Authorize(Roles = "Admin")]
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly UserManager<ApplicationUser> _userManager;
 
+        public IndexModel(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [BindProperty]
+        public IList<ApplicationUser> Utenti { get; set; }
+
+        public IActionResult OnGet()
+        {
+            Utenti = _userManager.GetUsersInRoleAsync("User").Result;
+
+            return Page();
         }
     }
 }
